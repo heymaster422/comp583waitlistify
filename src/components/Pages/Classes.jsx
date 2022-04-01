b5f0618f7d9d2349718e01464c580cdbae2fd155
ocansino <oscar.cansino.457@my.csun.edu>
2022-03-31 19:52:55 -0700

    class enrollment table

--- src/components/Pages/Classes.jsx
+++ src/components/Pages/Classes.jsx
@@ -9,7 +9,53 @@ export default function Classes() {
         
         <div>
             <Nav />
-        <h1>Hello World Classes</h1>
+        <h1>Class Enrollment</h1>
+            <div className = "table">
+                <h2>Class List</h2>
+                <table>
+                    <tr>
+                        <th>Class Title</th>
+                        <th>Professor</th>
+                        <th>Enroll</th>
+                        <th>Drop</th>
+                    </tr>
+                    <tr>
+                        <td>Comp 110</td>
+                        <td>Dewey</td>
+                        <td>
+                            <button type="button">Enroll</button>
+                        </td>
+                        <td>
+                            <button type="button">Drop</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Comp 333</td>
+                        <td>Zamanifar</td>
+                        <td>
+                            <button type="button">Enroll</button>
+                        </td>
+                        <td>
+                            <button type="button">Drop</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Comp 583</td>
+                        <td>Wang</td>
+                        <td>
+                            <button type="button">Enroll</button>
+                        </td>
+                        <td>
+                            <button type="button">Drop</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+
+
         </div>
+        
     )
+   
 }
