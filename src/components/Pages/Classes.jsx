772738064f02f7f6011405ed5866f2b46bdea607
ocansino <oscar.cansino.457@my.csun.edu>
2022-04-02 18:36:36 -0700

    fixed counter

--- src/components/Pages/Classes.jsx
+++ src/components/Pages/Classes.jsx
@@ -4,13 +4,18 @@ import Nav from "../Nav/Nav";
 
 
 export default function Classes() {
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const [count3, setCount3] = useState(0);
 
     return(
         
         <div>
             <Nav />
+            
         <h1>Class Enrollment</h1>
-            <div className = "table">
+        
+            <div class= "table">
                 <h2>Class List</h2>
                 <table>
                     <tr>
@@ -18,44 +23,72 @@ export default function Classes() {
                         <th>Professor</th>
                         <th>Enroll</th>
                         <th>Drop</th>
+                        <th>Students</th>
                     </tr>
                     <tr>
                         <td>Comp 110</td>
                         <td>Dewey</td>
-                        <td>
-                            <button type="button">Enroll</button>
+                        <td>      
+                            <div className="my-5">
+                                            
+                                <button id="btn btn-success mx-3" type="button" onClick={() => setCount(count + 1)}>Enroll</button>
+                            </div>       
                         </td>
                         <td>
-                            <button type="button">Drop</button>
+                            <div className="my-5">
+                                <button id="btn btn-danger mx-3" type="button" onClick={() => setCount(count - 1)}>Drop</button>
+                            </div> 
+                        </td>
+                        <td>
+                        <h2 className="my-5">{count}</h2>
                         </td>
                     </tr>
                     <tr>
                         <td>Comp 333</td>
                         <td>Zamanifar</td>
-                        <td>
-                            <button type="button">Enroll</button>
+                        <td>      
+                            <div className="my-5">
+                                            
+                                <button id="btn btn-success mx-3" type="button" onClick={() => setCount2(count2 + 1)}>Enroll</button>
+                            </div>       
                         </td>
                         <td>
-                            <button type="button">Drop</button>
+                            <div className="my-5">
+                                <button id="btn btn-danger mx-3" type="button" onClick={() => setCount2(count2 - 1)}>Drop</button>
+                            </div> 
+                        </td>
+                        <td>
+                        <h2 className="my-5">{count2}</h2>
                         </td>
                     </tr>
                     <tr>
                         <td>Comp 583</td>
                         <td>Wang</td>
-                        <td>
-                            <button type="button">Enroll</button>
+                        <td>      
+                            <div className="my-5">
+                                            
+                                <button id="btn btn-success mx-3" type="button" onClick={() => setCount3(count3 + 1)}>Enroll</button>
+                            </div>       
                         </td>
                         <td>
-                            <button type="button">Drop</button>
+                            <div className="my-5">
+                                <button id="btn btn-danger mx-3" type="button" onClick={() => setCount3(count3 - 1)}>Drop</button>
+                            </div> 
+                        </td>
+                        <td>
+                        <h2 className="my-5">{count3}</h2>
                         </td>
                     </tr>
                 </table>
             </div>
-
+            
+            
 
 
         </div>
         
+        
     )
+    
    
 }
